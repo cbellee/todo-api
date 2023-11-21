70fa382aa25cd4ec99faaf5f7bd274ec14455a7f
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2023-11-22 09:40:45 +1100

    removed whitespace

--- ui/env.sh
+++ ui/env.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# https://www.freecodecamp.org/news/how-to-implement-runtime-environment-variables-with-create-react-app-docker-and-nginx-7f9d42a91d70/
+
 echo "window._env_ = {" > ./env-config.js
 awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env-config.js
 echo "}" >> ./env-config.js
